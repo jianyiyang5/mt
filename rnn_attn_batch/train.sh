40d5858a3de7975f358aca26e8a9cc313dc38284
jianyi_yang <jianyi_yang@apple.com>
2019-09-03 22:59:11 -0700

    transformer done

--- rnn_attn_batch/train.sh
+++ rnn_attn_batch/train.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
-python split_data.py
+#pip install subword-nmt
+#python split_data.py
 python train.py
-python eval.py
-perl multi-bleu.perl test/test.en < test/test.hyp
+#python eval.py
+#perl multi-bleu.perl test/test.en < test/test.hyp
