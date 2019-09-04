83447e12122e125e884ecf6568b1faf8e4bcaa63
jianyi_yang <jianyi_yang@apple.com>
2019-09-04 11:24:21 -0700

    bpe

--- rnn_attn_batch/bpe.sh
+++ rnn_attn_batch/bpe.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-cut -f1 data/eng-fra.raw > eng.txt
-cut -f2 data/eng-fra.raw > fra.txt
-subword-nmt learn-bpe -s 13000 < eng.txt > eng.dict
-subword-nmt apply-bpe -c eng.dict < eng.txt > eng.bpe
-subword-nmt learn-bpe -s 13000 < fra.txt > fra.dict
-subword-nmt apply-bpe -c fra.dict < fra.txt > fra.bpe
-paste eng.bpe fra.bpe > eng-fra.bpe
+cut -f1 data/eng-fra.raw > data/eng.txt
+cut -f2 data/eng-fra.raw > data/fra.txt
+subword-nmt learn-bpe -s 13000 < data/eng.txt > data/eng.dict
+subword-nmt apply-bpe -c data/eng.dict < data/eng.txt > data/eng.bpe
+subword-nmt learn-bpe -s 13000 < data/fra.txt > data/fra.dict
+subword-nmt apply-bpe -c data/fra.dict < data/fra.txt > data/fra.bpe
+paste data/eng.bpe data/fra.bpe > data/eng-fra.bpe
