d14b0dc04d0664dc4beb9483f964e6923947a752
jianyi_yang <jianyi_yang@apple.com>
2019-09-11 23:51:40 -0700

    access internal module

--- rnn_attn_batch/train.py
+++ rnn_attn_batch/train.py
@@ -39,8 +39,7 @@ def train(device, input_variable, lengths, target_variable, mask, max_target_len
     # Forward pass through encoder
     print('debug 40 input_variable size=', input_variable.size())
     print('debug 40 lengths size=', lengths.size())
-    curMaxLen = torch.max(lengths)
-    encoder_outputs, encoder_hidden = encoder(input_variable, lengths, device, curMaxLen)
+    encoder_outputs, encoder_hidden = encoder(input_variable, lengths)
     print('debug encoder_outputs size=', encoder_outputs.size())
     print('debug encoder_hidden size=', encoder_hidden.size())
 
